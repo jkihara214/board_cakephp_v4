9ab0f08ef0f65a224fa14b9fb85eea35af6a8fd6
jkihara <jkihara214@gmail.com>
2022-12-06 16:49:07 +0900

    [Fix login display][scope] ログイン画面の文字の修正 <BLANK LINE> <body>

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -3,7 +3,7 @@
     <h3>Login</h3>
     <?= $this->Form->create() ?>
     <fieldset>
-        <legend><?= __('Please enter your username and password') ?></legend>
+        <legend><?= __('Please enter your Email and password') ?></legend>
         <?= $this->Form->control('email', ['required' => true]) ?>
         <?= $this->Form->control('password', ['required' => true]) ?>
     </fieldset>
